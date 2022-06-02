07bf6bbd66af62a86f97206cf14f115a0fab4964
reindrairawan <reindrairawan@gmail.com>
2022-06-02 19:05:03 +0700

    validasi looping untuk mendeteksi ke akuratan frame

--- metodeharcascade.py
+++ metodeharcascade.py
@@ -1,3 +1,5 @@
+import cv2
+from time import sleep
 
 pathcascade = 'cars.xml'
 pathvideo = 'video.mp4'
@@ -15,12 +17,14 @@ car=0
 z=0
 counter_line_positon = 550
 
+
 def center_handle(x,y,w,h):
     x1=int(w/2)
     y1=int(h/2)
     cx= x+x1
     cy= y+y1
     return cx,cy
+
 
 
 while True:
@@ -35,4 +39,44 @@ while True:
 
   
     # z = z+1
+       
     gray = cv2.cvtColor(framecars, cv2.COLOR_BGR2GRAY)
+
+    cars = cascade.detectMultiScale(gray, 1.1, 1)
+    cv2.line(framecars, (25, counter_line_positon), (1200, counter_line_positon), (255,127,0),3)
+   
+
+    
+    for (x,y,w,h) in cars:
+        cv2.rectangle(framecars,(x,y),(x+w,y+h), (0,255,0),2)
+        center = center_handle(x,y,w,h)
+        car_img = framecars[y:y+h,x:x+w]
+        z=z+1
+        
+        detect.append(center)
+        # cv2.circle(framecars, center, 4, (0,0,255), -1)
+
+        for (x,y) in detect:
+            if y<(counter_line_positon+offset) and y>(counter_line_positon-offset):
+                car+=1
+
+
+        
+        cv2.line(framecars,(25,counter_line_positon),(1200,counter_line_positon),(0,127,255),3)
+        # cv2.imwrite(crop_dir+ str(i) +'--' +str(z)+'.jpg',car_img)
+        
+        detect.remove((x,y))
+        print("Hitung Kendaraan:"+str(car))
+
+
+        cv2.putText(framecars, "Hitung:" +str(car), (450,70), cv2.FONT_HERSHEY_COMPLEX,2,(0,0,255),5)
+
+        cv2.imshow('video', framecars)
+
+   
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+ 
+
+cv2.destroyAllWindows()
+cap.release()
